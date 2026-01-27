93065d2fe77f6bb10302279d7cf20374400c3b7e
jbanks222 <jalenbanks123@gmail.com>
2026-01-27 13:04:58 -0500

    second

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << endl;
+
+    auto word = "Hello World";
+    cout << word << endl;
+
+    cout << endl;
+    return 0;
+}
---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << endl;
+
+    int x;
+    cout << "Enter a value for x: ";
+    cin >> x;
+    cout << x << endl;
+
+    cout << endl;
+    return 0;
+}
