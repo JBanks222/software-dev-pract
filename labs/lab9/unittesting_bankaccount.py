e5bd950263680cabe24b31e0c3614b441c08e28d
JBanks222 <115024510+JBanks222@users.noreply.github.com>
2026-03-02 01:19:49 +0000

    lab9 completed

---
+++ labs/lab9/unittesting_bankaccount.py
@@ -0,0 +1,40 @@
+"""
+Jalen Banks
+Lab 9, Unit Testing - BankAccount
+Mar 1, 2026
+"""
+
+import unittest
+
+from bankaccount import BankAccount
+
+
+class TestBankAccount(unittest.TestCase):
+    def setUp(self):
+        self.account = BankAccount(100)
+
+    def test_initial_balance(self):
+        self.assertEqual(self.account.balance, 100)
+
+    def test_deposit_adds_to_balance(self):
+        self.account.deposit(50)
+        self.assertEqual(self.account.balance, 150)
+
+    def test_withdraw_subtracts_from_balance(self):
+        self.account.withdraw(40)
+        self.assertEqual(self.account.balance, 60)
+
+    def test_withdraw_more_than_balance_raises_exception(self):
+        with self.assertRaises(ValueError):
+            self.account.withdraw(150)
+
+    def test_sequence_of_deposits_and_withdrawals(self):
+        self.account.deposit(25)
+        self.account.withdraw(10)
+        self.account.deposit(35)
+        self.account.withdraw(20)
+        self.assertEqual(self.account.balance, 130)
+
+
+if __name__ == '__main__':
+    unittest.main()
