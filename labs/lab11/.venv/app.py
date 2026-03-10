a7514a707d19899eb2d54484747ac8fc21d18dfb
JBanks222 <115024510+JBanks222@users.noreply.github.com>
2026-03-10 17:41:59 +0000

    LAB11

---
+++ labs/lab11/.venv/app.py
@@ -0,0 +1,35 @@
+"""
+Jalen Banks
+Lab 11, 03 2026
+Introduction to Flask
+"""
+
+from flask import Flask, render_template
+
+"""
+Create n object 'app' from the flask module
+"""
+app = Flask(__name__)
+
+#set the routing to the main page
+# 'route' decorator is used to access the root URL
+@app.route('/')
+def index():
+    name = "Jalen"
+    fruits = ["Apple", "Banana", "Cherry"]
+    fruit = "Pineapple"
+    return render_template('index.html', username=name, listfruits=fruits)
+
+
+# endpoints refer to the name of the view in the app
+@app.route('/about')
+def about():
+    return '<h1>About us</h1>'
+@app.route('/quotes')
+def quotes():
+    return '<h1>Famous Quotes</h1><p>"The only limit to our realization of tomorrow is our doubts of today." - Franklin D. Roosevelt</p>'
+# set the 'app' to run if you excute this file directly (not imported as a module)
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+#LAB EXCERCISE - MAKE PAGE FOR ABOUT
