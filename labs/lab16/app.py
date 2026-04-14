efbaa2b69c8bbbaf4e6ec882a4729bb576876e7d
JBanks222 <115024510+JBanks222@users.noreply.github.com>
2026-04-14 17:45:43 +0000

    lab16

---
+++ labs/lab16/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+#--------------
+#database config
+#--------------
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'flaskuser'
+app.config['MYSQL_PASSWORD'] = 'password123'
+app.config['MYSQL_DB'] = 'todo_db'
+
+db = mysql.connector.connect(
+    host=app.config['MYSQL_HOST'],
+    user=app.config['MYSQL_USER'],
+    password=app.config['MYSQL_PASSWORD'],
+    database=app.config['MYSQL_DB']
+)
+
+#----------------
+# Home route
+#----------------
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_tasks', methods=['GET'])
+def get_tasks():
+    try:
+        with db.cursor(dictionary=True) as cursor:
+            cursor.execute("SELECT * FROM tasks")
+            tasks = cursor.fetchall()   
+            return jsonify(tasks)
+    except mysql.connector.Error as e:
+        return jsonify({"error": str(e)}), 500
+
+#----------------
+# add new task
+#----------------
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    task = data.get('task')
+
+    if not task:
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO tasks (task) VALUES (%s)", (task,))
+        db.commit()
+        cursor.close()
+        return jsonify({'status': 'success'})
+    else:
+        return jsonify({'status': 'error'})
+    
+    
+
+
+#----------------
+#debug
+#----------------
+if __name__ == '__main__':
+    app.run(debug=True)
